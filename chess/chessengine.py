c4c93dba36e7d21241b0076f27c59734fe8d79dd
Ananta <anantahatwar1@gmail.com>
2023-10-24 13:25:51 +0530

    peices are moving

--- chess/chessengine.py
+++ chess/chessengine.py
@@ -11,8 +11,36 @@ class gamestate():
             ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
         ]
         self.white = True
-        
         self.movelog = []
 
+    def makemove(self, move):
+        self.board[move.startrow][move.startcol] = "--"
+        self.board[move.endrow][move.endcol] = move.pieceMoved
+        self.movelog.append(move)
+        self.white = not self.white
 
+class move():
+    ranktorow = {
+        "1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0
+    }
+    rowstorank = {v: k for k, v in ranktorow.items()}
+
+    filetocol = {
+        "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7
+    }
+    colstofile = {v: k for k, v in filetocol.items()}
+
+    def __init__(self, startsq, endsq, board):
+        self.startrow = startsq[0]
+        self.startcol = startsq[1]
+        self.endrow = endsq[0]
+        self.endcol = endsq[1]
+        self.pieceMoved = board[self.startrow][self.startcol]
+        self.pieceCaptured = board[self.endrow][self.endcol]
+
+    def chessnotation(self):
+        return self.rankfile(self.startcol, self.startrow) + self.rankfile(self.endcol, self.endrow)
+
+    def rankfile(self, r, c):
+        return self.colstofile[c] + self.rowstorank[r]
 
--- chess/chessmain.py
+++ chess/chessmain.py
@@ -1,6 +1,6 @@
 import pygame as p
 import os
-from chessengine import gamestate
+from chessengine import gamestate, move
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -8,48 +8,72 @@ SQ = WIDTH // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
-def loadimg():
+
+def load_images():
     pieces = ["wp", "wr", "wn", "wb", "wk", "wq", "bp", "br", "bn", "bb", "bk", "bq"]
-    image_directory = "C:/Users/anant/OneDrive/Desktop/AI project/chess/img"  # Use forward slashes
+    image_directory = "C:/Users/anant/OneDrive/Desktop/AI project/chess/img"
     for piece in pieces:
-        image_path = image_directory + "/" + piece + ".png"  # Use forward slashes
+        image_path = os.path.join(image_directory, piece + ".png")
         IMAGES[piece] = p.transform.scale(p.image.load(image_path), (SQ, SQ))
+
 
 def main():
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     gs = gamestate()
-    loadimg()
+    load_images()
     running = True
+    sqselected = ()
+    player_click = []
 
-    while running:  # Main game loop
+    while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // SQ
+                row = location[1] // SQ
 
-        drawgamestate(screen, gs)
+                if sqselected == (row, col):
+                    sqselected = ()
+                    player_click = []
+                else:
+                    sqselected = (row, col)
+                    player_click.append(sqselected)
+                if len(player_click) == 2:
+                    chess_move = move(player_click[0], player_click[1], gs.board)
+                    print(chess_move.chessnotation())
+                    gs.makemove(chess_move)
+                    sqselected = ()
+                    player_click = []
+
+        draw_game_state(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
 
-def drawgamestate(screen, gs):
-    drawboard(screen)
-    drawpieces(screen, gs.board)
 
-def drawboard(screen):
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    draw_pieces(screen, gs.board)
+
+
+def draw_board(screen):
     colors = [p.Color('white'), p.Color("gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r + c) % 2]
             p.draw.rect(screen, color, p.Rect(c * SQ, r * SQ, SQ, SQ))
 
-def drawpieces(screen, board):
+
+def draw_pieces(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            peice = board[r][c]
-            if peice != "--":
-                screen.blit(IMAGES[peice],p.Rect(c * SQ, r * SQ, SQ, SQ))
-            
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ, r * SQ, SQ, SQ))
+
 
 if __name__ == "__main__":
     main()
